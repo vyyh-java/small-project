22b62443dc6f08ad02bfdb9d3e49162041fa4172
vyyh-java <leimin9001@gmail.com>
2026-07-09 21:57:24 +0800

    Integrate goalRouter and enhance error handling
    improve structure.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const path = require("path");
-const fs = require("fs");
 require("dotenv").config();
 const pinoHttp = require('pino-http');
 const session = require('express-session');
 const sanitize = require('mongo-sanitize');
 const mongoose = require("mongoose");
+const goalRouter = require('./routes/goalRouter');
 const app = express();
 const PORT = 3000;
 
@@ -17,17 +17,13 @@ mongoose.connect(MONGODB_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Connection failed:", err));
 
-//!important
-const goalRouter = require('./routes/goalRouter');
-
 // Middleware
+app.use(pinoHttp()); //logger
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static('./'));
-app.use(pinoHttp()); //logger
 
-//user login simulation
+// user login simulation
 app.use(session({
   secret: process.env.SESSION_SECRET || 'dev-secret',
   resave: false,
@@ -35,23 +31,31 @@ app.use(session({
   cookie: { httpOnly: true, maxAge: 1000 * 60 * 60 * 24 }
 }));
 
-//!important
+// avoid noSQL injection
 app.use((req, res, next) => {
   if (req.body) req.body = sanitize(req.body);
   next();
 });
+
+// goal mini middleware
+app.use('/api/goals',goalRouter);
+
 // user login simulation
 app.get('/test-login', (req, res) => {
   req.session.userId = new mongoose.Types.ObjectId('000000000000000000000001');
   res.json({ message: 'Session set', userId: req.session.userId });
 });
+
 app.get('/profile', (req, res) => {
   if (!req.session.userId) return res.status(401).json({ message: 'Unauthorized' });
   res.json({ userId: req.session.userId, joinDate: '2026-01-12' });
 });
-// !important
-app.use('/api/goals',goalRouter);
 
+// error
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Server error', error: err.message });
+});
 
 // Start server
 app.listen(PORT, function() {
